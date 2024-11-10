606081708909234d082fa65afd4cd24dc2f0c905
Quzey35 <kzy.akcay@outlook.com>
2024-11-10 19:14:44 +0300

    Add files via upload

---
+++ import discord.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+import random
+
+# Bot token'inizi buraya ekleyin
+TOKEN = 'MTMwNTE5NTM0NjAzMjU5MDg2OA.GIJYnh.rkBRHw6ot0X3uTYpFbozN9BckE-Ad9-O56ooGI'
+
+# Intents ayarını yapıyoruz
+intents = discord.Intents.default()
+intents.message_content = True
+
+# Botu oluşturuyoruz
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} olarak giriş yaptım')
+
+@bot.command(name='yazıtura')
+async def yazitura(ctx):
+    sonuc = random.choice(["Yazı", "Tura"])
+    await ctx.send(f'{ctx.author.mention}, Sonuç: **{sonuc}**!')
+
+@bot.command(name='tahmin')
+async def tahmin(ctx, tahmin: int):
+    if tahmin < 1 or tahmin > 10:
+        await ctx.send(f'{ctx.author.mention}, lütfen 1 ile 10 arasında bir sayı tahmin edin.')
+    else:
+        dogru_sayi = random.randint(1, 10)
+        if tahmin == dogru_sayi:
+            await ctx.send(f'{ctx.author.mention}, Tebrikler! Doğru tahmin ettiniz: **{dogru_sayi}** 🎉')
+        else:
+            await ctx.send(f'{ctx.author.mention}, Maalesef yanlış. Doğru cevap: **{dogru_sayi}**.')
+
+bot.run(TOKEN)
